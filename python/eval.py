87fc8e7402ecacc2d4d3b15a5009b281a78b9ae8
Jack <jack.grahl@yahoo.co.uk>
2016-09-06 00:05:57 +0100

    Correctly implement this function.

--- python/eval.py
+++ python/eval.py
@@ -1,2 +1,10 @@
+from subprocess import check_output
+from tempfile import NamedTemporaryFile
+
 def get_output(code):
-    return code
+    tmpfile = NamedTemporaryFile()
+    tmpfile.write(code)
+    tmpfile.flush()
+    args = ['python', tmpfile.name]
+    output = check_output(args)
+    return output
